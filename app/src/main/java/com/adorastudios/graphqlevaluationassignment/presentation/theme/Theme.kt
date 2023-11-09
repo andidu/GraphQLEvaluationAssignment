da6695f19425d77b6589865e91a7f15d500e7100
andidu <andiduorange@gmail.com>
2023-11-09 19:50:57 +0100

    Initial commit

---
+++ app/src/main/java/com/adorastudios/graphqlevaluationassignment/presentation/theme/Theme.kt
@@ -0,0 +1,33 @@
+package com.adorastudios.graphqlevaluationassignment.presentation.theme
+
+import android.app.Activity
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val lightColors = lightColorScheme()
+
+@Composable
+fun GraphQLEvaluationAssignmentTheme(
+    content: @Composable () -> Unit,
+) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = lightColors.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = lightColorScheme(),
+        typography = Typography(),
+        content = content,
+    )
+}
